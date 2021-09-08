1364453f2036c9f576287992e4d1817d7b9da8ca
Orion Lawlor <lawlor@alaska.edu>
2021-09-07 21:04:14 -0800

    Add Euler angle keyboard rotation (for comparison).

--- Scripts/KeyboardRotateEuler.cs
+++ Scripts/KeyboardRotateEuler.cs
@@ -1,16 +1,16 @@
 /*
- Keyboard-based object rotation using Quaternion.
+ Keyboard-based object rotation using Euler angles.
 
- Works great, giving object-relative rotation, 
- at all angles (unlike Euler). 
- 
+ Works fairly OK until you rotate by 90 degrees, then
+ you get control inversion or gimbal lock.
+
  Orion Lawlor, lawlor@alaska.edu, 2021-09-07 (Public Domain)
 */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyboardRotateQuaternion : MonoBehaviour
+public class KeyboardRotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -23,8 +23,7 @@ public class KeyboardRotateQuaternion : MonoBehaviour
     {
         float rate=20.0f; // degrees/sec
         float dt=Time.deltaTime; // seconds
-        
-        Vector3 angles=new Vector3(0,0,0);
+        Vector3 angles=transform.localEulerAngles;
         
         // pitch (nose up and down)
         if (Input.GetKey(KeyCode.W)) angles.z += rate*dt;
@@ -38,15 +37,6 @@ public class KeyboardRotateQuaternion : MonoBehaviour
         if (Input.GetKey(KeyCode.Q)) angles.x += rate*dt;
         if (Input.GetKey(KeyCode.E)) angles.x -= rate*dt;
         
-        // Apply updated angles to our quaternion rotation:
-        Quaternion rot = transform.localRotation;
-        rot *= Quaternion.Euler(angles.x,angles.y,angles.z);
-        transform.localRotation=rot;
-        
-        // Apply ship-forward thrust with Z key:
-        Vector3 X=transform.right; // object's X axis (in world space)
-        Vector3 V=new Vector3(0,0,0);
-        if (Input.GetKey(KeyCode.Z)) V=X*100.0f;
-        transform.position += V*dt;        
+        transform.localEulerAngles=angles;
     }
 }
