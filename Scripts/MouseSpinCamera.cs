26ade11f65c0f1760bac957cbf6ab86025c54e7d
Dr. Lawlor <lawlor@alaska.edu>
2021-10-13 20:31:16 -0800

    Feature: allow mouse wheel zoom.

--- Scripts/MouseSpinCamera.cs
+++ Scripts/MouseSpinCamera.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class MouseSpinCamera : MonoBehaviour
 {
     public float speed=1.0f; // degrees of rotation per pixel of mouse movement
+    public float scaleSpeed=0.1f; // scale factor per pip of mouse scroll
 
     public float rotx=0.0f;
     public float roty=0.0f;
+    public float scale=1.0f;
 
     // Update is called once per frame
     void Update()
@@ -16,6 +18,8 @@ public class MouseSpinCamera : MonoBehaviour
             rotx += Input.GetAxis("Mouse X")*speed;
             roty += Input.GetAxis("Mouse Y")*speed;
         }
+        scale = scale*(1.0f-Input.mouseScrollDelta.y*scaleSpeed);
+        transform.localScale = new Vector3(scale,scale,scale);
         transform.localEulerAngles=new Vector3(roty,rotx,0);
     }
 }
